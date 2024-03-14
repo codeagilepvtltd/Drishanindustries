bc0f6d2db1c9e20f96e5b2611a2ce7689ef83210
codeagilepvtltd <157207503+codeagilepvtltd@users.noreply.github.com>
2024-03-14 12:32:58 +0530

    new page added for home

---
+++ Drishanindustries/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drishanindustries.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("FrontEnd/Index");
+        }
+    }
+}
--- Drishanindustries/Program.cs
+++ Drishanindustries/Program.cs
@@ -65,7 +65,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 //app.MapRazorPages();
 
