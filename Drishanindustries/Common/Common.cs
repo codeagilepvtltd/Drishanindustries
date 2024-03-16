7ae95a805745963cb866b2e0bc9f6ee1071590c5
codeagilepvtltd <157207503+codeagilepvtltd@users.noreply.github.com>
2024-03-16 16:08:56 +0530

    modifed the changes

--- Drishanindustries/Common/Common.cs
+++ Drishanindustries/Common/Common.cs
@@ -3,6 +3,7 @@
     public class Common
     {
         public const string AppName = "DrishanIndustries";
+        public const string AppUrl = "http://drishanindustries.com";
         public const string PageName = "{0} - {1}";
     }
 }
