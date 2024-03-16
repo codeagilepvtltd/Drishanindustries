e50329273b86fb8e34c3534315dd116cdcb2e521
codeagilepvtltd <157207503+codeagilepvtltd@users.noreply.github.com>
2024-03-16 17:42:27 +0530

    modified the code

--- Drishanindustries/Common/Common.cs
+++ Drishanindustries/Common/Common.cs
@@ -2,8 +2,8 @@
 {
     public class Common
     {
-        public const string AppName = "DrishanIndustries";
-        public const string AppUrl = "http://drishanindustries.com";
+        public const string AppName = "Eminent Devices";
+        public const string AppUrl = "http://EminentDevices.com";
         public const string PageName = "{0} - {1}";
     }
 }
